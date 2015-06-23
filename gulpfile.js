63dea09cb83fe834b9f425cacd212da985a5f369
Bhargav Patel <ziikutv@gmail.com>
2015-06-23 00:47:52 -0400

    Finish browersify/jsx transform task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    size = require('gulp-size'),
+    clean = require('gulp-clean');
+
+gulp.task('clean', function () {
+
+});
+
+
+gulp.task('transform', function () {
+  return gulp.src('./project/static/js/src/main.js')
+    .pipe(browserify({transform: ['reactify']}))
+    .pipe(gulp.dest('./project/static/js'))
+    .pipe(size());
+});
+
 
 gulp.task('default', function () {
   console.log("Hello, World!");
+  gulp.start('transform');
 });
