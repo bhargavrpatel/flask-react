6c91438cc9dc927c8380a6cb79f53edab4357011
Bhargav Patel <ziikutv@gmail.com>
2015-06-23 00:58:45 -0400

    Finish clean function in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp'),
-    browserify = require('gulp-browserify'),
-    size = require('gulp-size'),
-    clean = require('gulp-clean');
+    browserify = require('gulp-browserify');
 
 gulp.task('clean', function () {
-
+  return gulp.src(['./project/static/scripts/js'], {read: false})
+    .pipe(require('gulp-clean')());
 });
 
 
@@ -12,11 +11,11 @@ gulp.task('transform', function () {
   return gulp.src('./project/static/js/src/main.js')
     .pipe(browserify({transform: ['reactify']}))
     .pipe(gulp.dest('./project/static/js/dist/'))
-    .pipe(size());
+    .pipe(require('gulp-size')());
 });
 
 
-gulp.task('default', function () {
+gulp.task('default', ['clean'], function () {
   console.log("Hello, World!");
   gulp.start('transform');
 });
